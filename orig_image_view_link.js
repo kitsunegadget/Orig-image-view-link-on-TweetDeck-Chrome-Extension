6c1b8486f569fe525ab04200a565ab424e032fcb
yuzusiro <kitsunegadget@gmail.com>
2018-09-02 07:19:14 +0900

    use MurationObserver instead of eventListener for notify DOM changed.

--- orig_image_view_link.js
+++ orig_image_view_link.js
@@ -1,29 +1,37 @@
 //div.id "open-modal"のdisplayがblockにならないとmed-trayもmedia-imgも生成されないので,DOMの変更を監視。
-setTimeout(function() {
+setTimeout(() => {
     console.log("orig image view READY.");
     var open_modal = document.getElementById("open-modal")
 
-    open_modal.addEventListener("DOMSubtreeModified", function(){
-        var med_tray = document.getElementsByClassName("med-tray");
-        var id = document.getElementById("original-size"); //elementが生成されてなければnull
+    var config = { attribudes: true, childList: true, subtree: true };
 
-        if (open_modal.style.display === "block" && id === null){
-            element = addElement();
-            if (element != null){
-                med_tray[0].appendChild(element);
-                //console.log("apeendChild");
+    var observer = new MutationObserver((murationList) => {
+        for (var muration of murationList) {
+            if (muration.type == "childList") {
+
+                var med_tray = document.getElementsByClassName("med-tray");
+                var id = document.getElementById("original-size"); //elementが生成されてなければnull
+
+                if (open_modal.style.display === "block" && id === null) {
+                    element = addElement();
+                    if (element != null) {
+                        med_tray[0].appendChild(element);
+                        //console.log("apeendChild");
+                    }
+                }
             }
         }
     });
+    observer.observe(open_modal, config);
 }, 5000);
 
-function addElement(){
+function addElement() {
     var mediaImg = document.querySelector(".med-tray .media-img");
     if (mediaImg != undefined) {
         var mediaURL = mediaImg.src;
 
         //largeの文字を引いてorigを追加する。もしsmallだった場合も5文字なのでだいじょうぶ。
-        editURL = mediaURL.substr(0, mediaURL.length-5)+"orig";
+        editURL = mediaURL.substr(0, mediaURL.length - 5) + "orig";
         //console.log(editURL);
 
         var element = document.createElement("a");
@@ -32,7 +40,7 @@ function addElement(){
         element.href = editURL; //ここを画像リンクにする
         element.target = "_blank";
         element.id = "original-size"; //フラグ管理用にidをつける。状態が変わればelementは自動的に消える。
-        
+
         return element;
     }
     return null;
